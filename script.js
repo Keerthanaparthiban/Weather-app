1e31593dbfc2e8466c793763d88360f9366c2b05
Keerthanaparthiban <92458543+Keerthanaparthiban@users.noreply.github.com>
2023-05-31 00:08:10 +0530

    on window load access lat and long. fetch openweather api

---
+++ script.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        long = position.coords.longitude;
+        lat = position.coords.latitude;
+        const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=a55e102beeb2e059a11798410a6ad9d7
+        `;
+        fetch(api)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+          })
+          .then(data => {
+            console.log(data);
+          })
+          .catch(error => {
+            console.error("Error:", error);
+          });
+      },
+      error => {
+        console.error("Geolocation error:", error);
+      }
+    );
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+  }
+});
